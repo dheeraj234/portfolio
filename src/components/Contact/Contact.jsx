b6cabab919a2be31e5abe42b956174e8e1c51213
DHEERAJ KUMAR PAMPARI <44135491+dheeraj234@users.noreply.github.com>
2025-02-10 18:19:16 +0000

    chage-1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,22 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:dheerajkumarpampari@gmail.com">
+            dheerajkumarpampari@gmail.com
+          </a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/dheeraj156">
+            linkedin.com/in/dheeraj156
+          </a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/dheeraj234">github.com/dheeraj234</a>
         </li>
       </ul>
     </footer>
